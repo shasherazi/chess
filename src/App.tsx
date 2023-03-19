3b2a0eae9a99f41ef36b2fec52ac658ccf835b31
shasherazi <hassanrandomz@gmail.com>
2023-03-19 12:38:32 +0500

    Add `eslint` linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <div className="App">
-        <h1>
-            Chess
-        </h1>
+      <h1>Chess</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
