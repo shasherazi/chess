43a980a8d49eace959e5fecd428c93b67e15bb43
shasherazi <hassanrandomz@gmail.com>
2023-03-19 15:44:06 +0500

    God knows why the nav logo isnt getting sized accordingly

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,32 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
+import chessLogo from '../assets/images/chessLogo.png';
+import styles from '../styles/Nav.module.css';
 
 export default function Nav() {
   return (
     <>
-      <div>Nav</div>
+      <nav className={styles.nav}>
+        <ul className={styles.navList}>
+          <div className={styles.navLeft}>
+            <li className={styles.navItem}>
+              <div className={styles.imgDiv}>
+                <img src={chessLogo} alt="chess logo" className={styles.navLogo} />
+              </div>
+            </li>
+          </div>
+          <div className={styles.navRight}>
+            <li className={styles.navItem}>
+              <Link to="/" className={styles.navLink}>Home</Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link to="/board" className={styles.navLink}>Board</Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link to="/about" className={styles.navLink}>About</Link>
+            </li>
+          </div>
+        </ul>
+      </nav>
       <Outlet />
     </>
   );
