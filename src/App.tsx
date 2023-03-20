55f29ce20994be94b6aaafd07cfa9379aca7893b
shasherazi <hassanrandomz@gmail.com>
2023-03-20 23:43:08 +0500

    Add about component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Nav from './components/Nav';
 import Home from './components/Home';
 import './App.css';
 import Board from './components/Board';
+import About from './components/About';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
           <Route element={<Nav />}>
             <Route path="/" element={<Home />} />
             <Route path="/board" element={<Board />} />
+            <Route path="/about" element={<About />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/components/About.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/About.module.css';
+
+export default function About() {
+  return (
+    <div className={styles.about}>
+      <h1 className={styles.title}>About</h1>
+    </div>
+  );
+}
