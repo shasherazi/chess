c2bf2d66f552566633581323978f1ea9239b90bb
shasherazi <hassanrandomz@gmail.com>
2023-03-20 19:23:00 +0500

    Fix background image only appears on home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  useEffect(() => {
+    document.body.classList.add('background');
+
+    return () => {
+      document.body.classList.remove('background');
+    };
+  }, []);
+
   return (
     <div className={styles.home}>
       <Link to="/board" className={styles.button}>
